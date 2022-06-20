c7ee1e61d5b8a9ea77bf1cdacbded91d24880ce2
Leon <8demiurg@gmail.com>
2022-06-20 21:43:35 +0300

    Buttons, score and hide bars

--- app/src/main/java/com/example/mytetrisapplication/MainActivity.kt
+++ app/src/main/java/com/example/mytetrisapplication/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.mytetrisapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private var tvHighScore: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+        val btnNewGame = findViewById<Button>(R.id.btn_new_game)
+        val btnResetScore = findViewById<Button>(R.id.btn_reset_score)
+        val btnExit = findViewById<Button>(R.id.btn_exit)
+        tvHighScore = findViewById<TextView>(R.id.tv_high_score)
     }
 }
