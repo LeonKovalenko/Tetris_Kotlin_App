4d17cf4b98107ffce57a07dfbf63f7e991862935
Leon <8demiurg@gmail.com>
2022-06-21 01:08:34 +0300

    Buttons, score and hide bars. Fun of preference and storage score data

--- app/src/main/java/com/example/mytetrisapplication/MainActivity.kt
+++ app/src/main/java/com/example/mytetrisapplication/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.mytetrisapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import com.google.androidgamesdk.GameActivity
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,5 +21,20 @@ class MainActivity : AppCompatActivity() {
         val btnResetScore = findViewById<Button>(R.id.btn_reset_score)
         val btnExit = findViewById<Button>(R.id.btn_exit)
         tvHighScore = findViewById<TextView>(R.id.tv_high_score)
+        btnNewGame.setOnClickListener (this::onBtnNewGameClick)
+        btnResetScore.setOnClickListener (this::onBtnResetScoreClick)
+        btnExit.setOnClickListener (this::onBtnExitClick)
+    }
+    private fun onBtnNewGameClick(view: View) {
+        val intent = Intent(this, GameActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun onBtnResetScoreClick(view: View) {
+        TODO()
+    }
+
+    private fun onBtnExitClick(view: View) {
+        exitProcess(0)
     }
 }
